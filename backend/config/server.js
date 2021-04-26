2b88942cccd643c6313075d06c956b26a141f26e
David Lowe Larsson <david.lowelarsson@ur.se>
2021-04-26 23:18:11 +0200

    working project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6f37ba79812952ae68f70ac176df7542'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0dfbe6ef-dc6c-4584-a5c2-a7c6e89e27ae'
+};
